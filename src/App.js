29d8d02b3bd1018cb1b1ebeba1226f38c67050b1
binuli <65843276+binuli@users.noreply.github.com>
2021-04-09 12:18:42 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 
 //importing components
 import ProfContainer from "./Components/prof-container"
+import Navbar from "./Components/navbar"
 
 function App() {
   return (
     
       <div className="App">
+        {/* Navbar */}
+        <Navbar/>
+        {/* profile header */}
         <ProfContainer/>
       </div>
   );
---
+++ src/Components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./navbar.scss"
+
+
+function Navbar() {
+    return (
+        <div className="nav">
+            
+        </div>
+    )
+}
+
+export default Navbar
